3274f2e30c365a9e8df455878c50ac7fcb58b7b3
dean <zxxjjforever@163.com>
2020-10-09 20:22:40 +0800

    remove code

--- base_controller.go
+++ base_controller.go
@@ -1,18 +1,13 @@
 package catgo
 
-import (
-	"github.com/astaxie/beego"
-)
-
 // this is BaseController
 type BaseController struct {
-	beego.Controller
 }
 
-func (this *BaseController) SetTheme(theme string) {
-
-}
-
-func (this *BaseController) Display(file string) {
-	this.TplName = file + ".html"
-}
+//func (this *BaseController) SetTheme(theme string) {
+//
+//}
+//
+//func (this *BaseController) Display(file string) {
+//	this.TplName = file + ".html"
+//}
